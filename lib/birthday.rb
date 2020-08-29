a4c5e0c1091efdd8501eded139265c71f01308fe
Taylor Sieling <taylorasieling@gmail.com>
2020-08-29 02:03:34 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if "#{age}" > 12
+    if |age| > 12
       puts "You are too old for this."
     else 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
